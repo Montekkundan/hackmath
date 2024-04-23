33a666156db7e7327d9f00fb8bad51e36d1571e4
Montek <87750128+Montekkundan@users.noreply.github.com>
2024-04-22 21:53:57 -0700

    admin initial commit

---
+++ apps/admin/src/app/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Inter } from "next/font/google";
+import "jsvectormap/dist/css/jsvectormap.css";
+import "flatpickr/dist/flatpickr.min.css";
+import "@/css/satoshi.css";
+import "@/css/style.css";
+import { useEffect, useState } from "react";
+import Loader from "@/components/common/Loader";
+const inter = Inter({ subsets: ["latin"] });
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // const pathname = usePathname();
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? <Loader /> : children}
+        </div>
+      </body>
+    </html>
+  );
+}
